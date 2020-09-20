072f3767fb53eac7103bb4f5587699da08a8265c
Andrey <alian@mail.ru>
2020-09-20 12:31:55 +0700

    add output json

--- 1cfinder.ps1
+++ 1cfinder.ps1
@@ -1,9 +1,9 @@
 ﻿param (
-    [string]$plocate="C:\Users\andrey_2\desktop",
+    [string]$dir="C:\Users\andrey_2\desktop",
     [switch]$help=$false,
-    [switch]$debug=$true,
-    [string]$pfilename="*.txt",
-    [switch]$palldrivers=$false
+    [switch]$debug=$false,
+    [string]$filename="*.txt",
+    [switch]$alldrivers=$false
 )
 
 function GetAllDrivers()
@@ -18,12 +18,8 @@ function GetAllDrivers()
 
 function SearchInfobases($path, [string]$ext)
 {
-    Write-Debug "SearchInfobases:path: $path"
-    Write-Debug "SearchInfobases:ext: $ext"
-
-    Write-Debug "Get-ChildItem -Path $path -Include $ext -Recurse -ErrorAction SilentlyContinue  | Select FullName"
-    $dbs = Get-ChildItem -Path $path -Include "$ext" -Recurse -ErrorAction SilentlyContinue  | Select FullName
-    return($dbs)
+    
+    return Get-ChildItem -Path $path -Include "$ext" -Recurse -ErrorAction SilentlyContinue  | Select FullName
 
 }
 
@@ -35,40 +31,37 @@ if($debug){
 if($help){
     $h = "
     Finding 1c infobases on server
-    1cfinder [ -path path-to-folder ]
-    -path path - search infobases in path, default search in all drivers
-    -filename - filename, wildcard
+    1cfinder [ -path path-to-folder | -alldrivers ] -filename filename
+    -dir path - search infobases in path, default search in all drivers
+    -filename - filename, wildcard, default *.1CD
+    -alldrivers - find filename over all drivers
     ";
     Write-Output($h);
     Exit;
 }
 
 
-if($palldrivers){
-    $drivers =  GetAllDrivers
+if($alldrivers){
     
-    $d = SearchInfobases $drivers $pfilename
-
     Remove-Item -Path .\infobases.prev.csv -Force
     Rename-Item -Path .\infobases.csv -NewName .\infobases.prev.csv -Force
 
-    $d | ConvertTo-Csv  | Out-File -Encoding "UTF8" infobases.csv
+    $drivers =  GetAllDrivers
+    SearchInfobases $drivers $pfilename | ConvertTo-Csv  | Out-File -Encoding "UTF8" infobases.csv
 
-
-
+    
 
 }else{
-    
-    $d = SearchInfobases $plocate $pfilename
 
     Remove-Item -Path .\infobases.prev.csv -Force
     Rename-Item -Path .\infobases.csv -NewName .\infobases.prev.csv -Force
 
-    $d | ConvertTo-Csv  | Out-File -Encoding "UTF8" infobases.csv
+    SearchInfobases $plocate $pfilename | ConvertTo-Csv  | Out-File -Encoding "UTF8" infobases.csv
 
 }
 
 
 $prev = Import-Csv -Encoding UTF8 .\infobases.prev.csv
 $last = Import-Csv -Encoding UTF8 .\infobases.csv
-Compare-Object  $prev.FullName $last.FullName
+$cmp = Compare-Object  $prev.FullName $last.FullName
+$cmp | ConvertTo-Json
